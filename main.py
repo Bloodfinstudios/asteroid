4be084274d0f3846c43650f9a26439ea9691712c
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-12-21 12:24:06 -0700

    Added an FPS cap

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from logger import log_state
 def main():
 
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
     
     game_running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -23,7 +26,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
