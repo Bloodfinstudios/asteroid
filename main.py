59b67559c25d2837ff8dc23224e32dd5d5588dff
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-12-21 11:58:55 -0700

    Creates a black screen that stays on the screen until exited

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ def main():
 
     pygame.init()
     
+    game_running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    while game_running:
+        log_state()
+
+        screen.fill("black")
+        pygame.display.flip()
+
+
 
 
 if __name__ == "__main__":
