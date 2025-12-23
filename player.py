3647b8e45bb2b3817e4b60d8dd0db39d4b0771be
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-12-22 19:34:57 -0700

    Finished up working on Player.py file

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
-from constant import PLAYER_RADIUS
+import pygame
+from constant import PLAYER_RADIUS, LINE_WIDTH
 
 class Player(CircleShape):
     def __init__(self,x, y, rotation) 
@@ -13,3 +14,7 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+        
