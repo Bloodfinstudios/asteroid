5e8e41b38b5b531f1c4c49d9722be9af874ff7b4
Bloodfinstudios <bloodfinstudios@gmail.com>
2025-12-25 12:46:33 -0700

    Added Groups methods to the program

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ def main():
     dt = 0
     
     game_running = True
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
     while game_running:
         log_state()
         
@@ -25,14 +30,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        
+        for draws in drawable:
+            draws.draw(screen)
+       
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
-
 
 
 if __name__ == "__main__":
